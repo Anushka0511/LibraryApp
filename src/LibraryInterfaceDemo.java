9cf8f1d4f57116e814ec17c4dab0d54573b420c4
Anushka0511 <anushkagupta.ag2006@gmail.com>
2026-06-07 20:37:44 +0530

    Create LibraryInterfaceDemo.java

---
+++ src/LibraryInterfaceDemo.java
@@ -0,0 +1,22 @@
+// Demo class to test KidUser and AdultUser
+public class LibraryInterfaceDemo {
+    public static void main(String[] args) {
+        // Test Case 1: KidUser
+        KidUser kid1 = new KidUser(10, "Kids");
+        kid1.registerAccount();
+        kid1.requestBook();
+
+        KidUser kid2 = new KidUser(18, "Fiction");
+        kid2.registerAccount();
+        kid2.requestBook();
+
+        // Test Case 2: AdultUser
+        AdultUser adult1 = new AdultUser(5, "Kids");
+        adult1.registerAccount();
+        adult1.requestBook();
+
+        AdultUser adult2 = new AdultUser(23, "Fiction");
+        adult2.registerAccount();
+        adult2.requestBook();
+    }
+}
